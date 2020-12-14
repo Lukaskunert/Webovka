8f31071303a5c6a45789df9506b87a2dbf1d5d5d
Lukaskunert <36969999+Lukaskunert@users.noreply.github.com>
2020-12-14 10:42:17 +0100

    nešlonic

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,13 +5,17 @@ import { ContactsComponent } from './contacts/contacts.component';
 import { AboutComponent } from './about/about.component';
 import {UsersComponent} from './users/users.component';
 import {UserDetailComponent} from './user-detail/user-detail.component';
+import {UserCreateComponent} from './user-create/user-create.component';
+
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
   {path: 'contacts', component: ContactsComponent},
   {path: 'about', component: AboutComponent},
+  {path: 'user.create', component: UserCreateComponent},
   {path: 'users', component: UsersComponent},
   {path: 'user/:id', component: UserDetailComponent},
+
   {path: '**', redirectTo: ''}
 ];
 
