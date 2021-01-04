02ecdc64cc336fc0c7fc76d883684be13c04ddc8
Lukaskunert <36969999+Lukaskunert@users.noreply.github.com>
2021-01-04 09:35:04 +0100

    xd

--- src/app/users.service.ts
+++ src/app/users.service.ts
@@ -1,26 +1,45 @@
-import { Injectable } from '@angular/core';
-import {Observable, of} from 'rxjs';
+import {Injectable} from '@angular/core';
+import {Observable, of} from "rxjs";
 
 export interface IUserEntity {
-  id: number;
-  username: string;
+username: string;
+id: number;
+password: string;
 }
 @Injectable({
-  providedIn: 'root'
-})
+providedIn: 'root'})
 export class UsersService {
 
-  private users: IUserEntity[] = [
-    {id: 1, username: 'XXXgetrektXXX69CZSK'}
+private id = 1;
+private users: IUserEntity[] = [
+{id: 1, username: 'Lukyn', password: '123321'}
+];
 
-    ];
-  constructor() {
-
-  }
+constructor() {}
   getAllUsers(): Observable<IUserEntity[]> {
     return of(this.users);
   }
+  getUserById(id: number): Observable<IUserEntity> {
+    let user: IUserEntity;
+    for (const u of this.users) {
+      if (id === u.id) {
+        user = u;
+        break;
+      }
+    }return of(user);
+  }
+  create(newUsername: string, newPassword: string): Observable<IUserEntity> {
+    const newUser: IUserEntity = {id: this.id++, username: newUsername, password: newPassword};
+    this.users.push(newUser);
+    return of(newUser);
+  }
+  edit(id: number, username: string): Observable<IUserEntity> {
+    for(const usernameE of this.users) {
+      if(usernameE.id === id) {
+        usernameE.username = username
+        return of(usernameE);
+      }
+    }
+    return of(null);
+  }
 }
-
-
-NEŠLONIC
